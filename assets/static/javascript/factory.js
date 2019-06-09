16a63524e9010deadc41da241fa2a5ef5a45cb25
Grant <gth2103@columbia.edu>
2019-06-08 21:59:25 -0400

    added  hover events and bounce animation  to cart, bug resetting animation

--- assets/static/javascript/factory.js
+++ assets/static/javascript/factory.js
@@ -25,6 +25,29 @@ var Order = function(number, size,  container)  {
 	return order;
 }
 
+var focus =  function(){
+
+	$('.size').on('mouseover', function(){
+		$('#cart').addClass('focus');
+	});
+
+	$('.size').on('mouseout', function(){
+		$('#cart').removeClass('focus');
+	});
+}
+
+var select =  function(){
+
+	$('.size').on('mousedown', function(){
+	});
+}
+
+var unselect =  function(){
+
+	$('.size').on('mouseup', function(){
+	});
+}
+
 var setDraggable  = function(id) {
 
 	$(id).on("click", function(){
@@ -47,50 +70,61 @@ var setDroppable  = function(id) {
 
         	var id = $(ui.draggable).attr('id');
 
+        	$('#cart').addClass('cart-bounce');
+
         	if(id === 'small-cone'){
-        		$('.cone-1').html('<img id="small-cone" class="m-2" src="./assets/static/images/icecream.png">');
+        		$('.cone-1').html('<img id="small-cone" class="m-2 size" src="./assets/static/images/icecream.png">');
         		$('.cone-1').ready('#small-cone', setDraggable(scone));
+        		$('.cone-1').ready('#small-cone', focus());
 
         		var new_order = Order(++order_number, 0, 0);
         		orders.push(new_order);
         	}
         	else if(id === 'medium-cone'){
-        		$('.cone-2').html('<img id="medium-cone" class="m-2" src="./assets/static/images/icecream-cone-2.png">');
+        		$('.cone-2').html('<img id="medium-cone" class="m-2 size" src="./assets/static/images/icecream-cone-2.png">');
         		$('.cone-2').ready('#medium-cone', setDraggable(mcone));
+        		$('.cone-2').ready('#medium-cone', focus());
 
         		var new_order = Order(++order_number, 1, 0);
         		orders.push(new_order);
 
         	}
         	else if(id === 'large-cone'){
-        		$('.cone-3').html('<img id="large-cone" class="m-2" src="./assets/static/images/icecream-cone-3.png">');
+        		$('.cone-3').html('<img id="large-cone" class="m-2 size" src="./assets/static/images/icecream-cone-3.png">');
         		$('.cone-3').ready('#large-cone', setDraggable(lcone));
+        		$('.cone-3').ready('#large-cone', focus());
 
         		var new_order = Order(++order_number, 2, 0);
         		orders.push(new_order);
         	}
         	else if(id === 'small-cup'){
-        		$('.cup-1').html('<img id="small-cup" class="m-2" src="./assets/static/images/bowl-1.tiff">');
+        		$('.cup-1').html('<img id="small-cup" class="m-2 size" src="./assets/static/images/bowl-1.tiff">');
         		$('.cup-1').ready('#small-cup', setDraggable(scup));
+        		$('.cup-1').ready('#small-cup', focus());
 
         		var new_order = Order(++order_number, 0, 1);
         		orders.push(new_order);
         	}
         	else if(id === 'medium-cup'){
-        		$('.cup-2').html('<img id="medium-cup" class="m-2" src="./assets/static/images/bowl-2.tiff">');
+        		$('.cup-2').html('<img id="medium-cup" class="m-2 size" src="./assets/static/images/bowl-2.tiff">');
         		$('.cup-2').ready('#medium-cup', setDraggable(mcup));
+        		$('.cup-2').ready('#medium-cup', focus());
 
         		var new_order = Order(++order_number, 1, 1);
         		orders.push(new_order);
 
         	}
         	else if(id === 'large-cup'){
-        		$('.cup-3').html('<img id="large-cup" class="m-2" src="./assets/static/images/bowl-3.tiff">');
+        		$('.cup-3').html('<img id="large-cup" class="m-2 size" src="./assets/static/images/bowl-3.tiff">');
         		$('.cup-3').ready('#large-cup', setDraggable(lcup));
+        		$('.cup-3').ready('#large-cup', focus());
 
         		var new_order = Order(++order_number, 2, 1);
         		orders.push(new_order);
         	}
+        	setTimeout(function(){
+				$('#cart').removeClass('cart-bounce');
+			}, 1000);
 
            console.log(orders)
         }
@@ -107,6 +141,10 @@ $('document').ready(function(){
 	setDraggable(lcup);
 
 	setDroppable(cart);
+
+	focus();
+	select();
+	unselect();
 
     window.onscroll = function() {
         var currentScrollPos = window.pageYOffset;
